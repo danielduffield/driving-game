6ba237f4792db0ad079707cb943c89013b821d1d
Daniel Duffield <danieljduffield@gmail.com>
2017-06-29 16:12:03 -0700

    Created a car class with properties for direction, speed, and location, as well as methods for turn, accelerate, deccelerate, and move. #1

---
+++ main.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+
+class Car {
+  constructor(direction, speed, location) {
+    this.direction = direction
+    this.speed = speed
+    this.location = location
+  }
+  turn(direction) {
+    this.direction = direction
+  }
+  accelerate(acceleration) {
+    this.speed += acceleration
+  }
+  move() {
+    // using traditional coordinate system with (0, 0) at center
+    switch (this.direction) {
+      case 'north':
+        this.location[0] += this.speed
+        break
+      case 'north-east':
+        this.location[0] += getSideFromHypotenuse(this.speed)
+        this.location[1] += getSideFromHypotenuse(this.speed)
+        break
+      case 'north-west':
+        this.location[0] += getSideFromHypotenuse(this.speed)
+        this.location[1] -= getSideFromHypotenuse(this.speed)
+        break
+      case 'south':
+        this.location[0] -= this.speed
+        break
+      case 'south-east':
+        this.location[0] -= getSideFromHypotenuse(this.speed)
+        this.location[1] += getSideFromHypotenuse(this.speed)
+        break
+      case 'south-west':
+        this.location[0] -= getSideFromHypotenuse(this.speed)
+        this.location[1] -= getSideFromHypotenuse(this.speed)
+        break
+      case 'east':
+        this.location[1] += this.speed
+        break
+      case 'west':
+        this.location[1] -= this.speed
+        break
+    }
+  }
+}
+
+function getSideFromHypotenuse(hypot) {
+  return Math.floor(((hypot ** 2) / 2) ** 0.5)
+}
